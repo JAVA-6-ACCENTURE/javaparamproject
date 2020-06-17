486a84bfecb1b9ba78210a4e169fbe715b14e2cd
madluk-me <m.lukasevica@gmail.com>
2020-06-17 21:03:26 +0300

    add new file with name MadaraLuk

---
+++ MadaraLuk/X2.java
@@ -0,0 +1,10 @@
+public class JavaArgumentProgram{
+	public static void main (String[] args){
+		String text = new String ("abc");
+			System.out.println("Your text: "+text.toUpperCase());
+		
+	
+
+	
+	}
+}
