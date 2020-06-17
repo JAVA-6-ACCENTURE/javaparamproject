580da2f187fa79173a209dca69017c232b137e4e
madluk-me <m.lukasevica@gmail.com>
2020-06-17 21:13:58 +0300

    MadaraLuk

--- MadaraLuk/X2.java
+++ MadaraLuk/X2.java
@@ -2,9 +2,5 @@ public class JavaArgumentProgram{
 	public static void main (String[] args){
 		String text = new String ("abc");
 			System.out.println("Your text: "+text.toUpperCase());
-		
-	
-
-	
 	}
 }
