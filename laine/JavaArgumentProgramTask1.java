e81ba8617c05ef2c26be579bb050639dc70c565c
LainBal <aleponijs@gmail.com>
2020-06-17 20:51:42 +0300

    laines darbs

---
+++ laine/JavaArgumentProgramTask1.java
@@ -0,0 +1,9 @@
+public class JavaArgumentProgramTask1{
+	
+	public static void main(String[] args){
+		String numArg = args[0];		
+		System.out.println("First argument value is: "+numArg.toUpperCase());
+				
+	}
+	
+}
