c50d5b323e9c357b8f8651d505ecdba2c58fc9f5
Elena <elena.shishova@gmail.com>
2020-06-17 21:03:20 +0300

    drukā otrādi

--- ElenaMion/ElenaArgPr.java
+++ ElenaMion/ElenaArgPr.java
@@ -1,5 +1,6 @@
 public class ElenaArgPr{
 	public static void main(String[] args){
-		System.out.println(args[0].toUpperCase());
+		for (int i = args[0].length(); i>0; i--)
+		System.out.print(args[0].charAt(i-1));
 	}
 }
