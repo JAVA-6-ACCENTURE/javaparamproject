230e194eab0fc668df661a0d4a47a1d466bf6435
Elena <elena.shishova@gmail.com>
2020-06-17 21:08:12 +0300

    refactored

--- ElenaMion/ElenaArgPr.java
+++ ElenaMion/ElenaArgPr.java
@@ -1,6 +1,7 @@
 public class ElenaArgPr{
 	public static void main(String[] args){
-		for (int i = args[0].length(); i>0; i--)
-		System.out.print(args[0].charAt(i-1));
+		for (int i = args[0].length()-1; i>=0; i--){
+			System.out.print(args[0].charAt(i));
 	}
+    }
 }
